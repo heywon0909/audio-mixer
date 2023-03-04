d8e1ffde3e6551af39f4887f470cfa384c8ea95d
heywon0909 <hw7319@naver.com>
2023-03-04 13:42:04 +0900

    audio 시각화

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
 
+const inputElem = document.getElementById('input');
+inputElem.addEventListener('change', function (e) {
+    let files = e.target.files;
+    readFile(files, this);
+
+});
+
+// audio 파일 읽어오기
+function readFile(files,target) {
+    let fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(files[0]);
+    fileReader.onload = function (e) {
+        let label = target.parentElement.querySelector('.label')
+        label.textContent = files[0].name;
+        label.dataset.jsLabel = true;
+        setAudioFile(e.target.result);
+    }
+
+}
+
+function setAudioFile(file) {
+    let context = new window.AudioContext();
+    context.decodeAudioData(file, function (buffer) {
+        let source = context.createBufferSource();
+        source.buffer = buffer;
+        source.loop = false;
+        source.connect(context.destination);
+        source.start(0);
+        setAudioVisualization(source, context);
+    })
+}
+
+function setAudioVisualization(source,context) {
+    const analyser = context.createAnalyser();
+    source.connect(analyser);
+    const distortion = context.createWaveShaper();
+    source.connect(context.destination);
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+    
+    var canvas = document.querySelector(".visualizer");
+    var canvasCtx = canvas.getContext("2d");
+
+
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+
+     function draw() {
+        let drawVisual = requestAnimationFrame(draw);  
+        analyser.getByteTimeDomainData(dataArray);
+        
+        canvasCtx.fillStyle = 'white';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = 'red';
+        canvasCtx.beginPath();
+
+
+        var sliceWidth = WIDTH * 1.0 / bufferLength;
+        var x = 0;
+
+        for(var i = 0; i < bufferLength; i++) {
+
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT/2;
+
+            if(i === 0) {
+            canvasCtx.moveTo(x, y);
+            } else {
+            canvasCtx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+        
+        canvasCtx.lineTo(canvas.width, canvas.height/2);
+        canvasCtx.stroke();
+
+    }
+    draw();
+}
+
+
+   
