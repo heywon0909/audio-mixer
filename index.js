32fdfa992c3f3a025a18bde02603efc73313342e
hw7319 <hw7319@naver.com>
2023-03-09 16:23:53 +0900

    현재 재생 시간 및 음악 재생 시간 표출 작업중...

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ function setAudioFile(file) {
     source = context.createBufferSource();
     source.buffer = buffer;
     source.loop = false;
-    
-   
+
     const gainNode = context.createGain();
     const pannerOptions = { pan: 0 };
     const panner = new StereoPannerNode(context, pannerOptions);
@@ -33,138 +32,161 @@ function setAudioFile(file) {
     let musicDelay = context.createDelay(5.0);
     const convolverNode = context.createConvolver();
     convolverNode.buffer = buffer;
-    
+
     // 사운드 수정하기
     let fast_rate = 1;
     const soundForward = document.getElementById("forward");
-    soundForward.addEventListener('click', function () {
-      fast_rate+=0.5;
+    soundForward.addEventListener("click", function () {
+      fast_rate += 0.5;
       source.playbackRate.value = fast_rate;
     });
 
-    
     const soundBackword = document.getElementById("backward");
-    soundBackword.addEventListener('click', function () {
+    soundBackword.addEventListener("click", function () {
       fast_rate -= 0.05;
-     source.playbackRate.value = fast_rate;
-    })
-    
-   
-    // const volumeControl = document.getElementById("volume");
-    // volumeControl.addEventListener("input", function (event) {
-    //   console.log('e', event.target.value);
-    //   gainNode.gain.value = event.target.value;
-    //   source.connect(gainNode);
-    //   gainNode.connect(context.destination);
-    //   console.log('gainNode',gainNode)
-    //   },false);
-    
-    const reverbControl = document.getElementById('reverb');
-    reverbControl.addEventListener('input', function () {
+      source.playbackRate.value = fast_rate;
+    });
+
+    const volumeControl = document.getElementById("volume");
+    volumeControl.addEventListener(
+      "input",
+      function (event) {
+        gainNode.gain.value = event.target.value;
+        source.connect(gainNode);
+        gainNode.connect(context.destination);
+      },
+      false
+    );
+
+    const reverbControl = document.getElementById("reverb");
+    reverbControl.addEventListener("input", function () {
       convolverNode.gain.value = this.value;
-    })
-   
+    });
+
     // 스테레오 패닝
-    
-      const pannerControl = document.getElementById("panner");
-      pannerControl.addEventListener("input", function () {
-        panner.pan.value = this.value;
+
+    const pannerControl = document.getElementById("panner");
+    pannerControl.addEventListener("input", function () {
+      panner.pan.value = this.value;
+    });
+
+    let oscillatorNode = null;
+
+    document
+      .getElementById("oscillator_control")
+      .addEventListener("click", function () {
+        if (this.dataset.osPlaying === "true") {
+          oscillatorNode.stop();
+          this.dataset.osPlaying = "false";
+        } else {
+          oscillatorNode = context.createOscillator();
+          oscillatorNode.connect(context.destination);
+          oscillatorNode.type = `${
+            document.querySelector("[name=os_control]:checked")?.id
+          }`;
+          oscillatorNode.start();
+          this.dataset.osPlaying = "true";
+        }
       });
-    
-    
-      let oscillatorNode = null;
 
-      document
-        .getElementById("oscillator_control")
-        .addEventListener("click", function () {
-          if (this.dataset.osPlaying === "true") {
-            oscillatorNode.stop();
-            this.dataset.osPlaying = "false";
-          } else {
-            oscillatorNode = context.createOscillator();
-            oscillatorNode.connect(context.destination);
-            oscillatorNode.type = `${
-              document.querySelector("[name=os_control]:checked")?.id
-            }`;
-            oscillatorNode.start();
-            this.dataset.osPlaying = "true";
-          }
-        });
+    document
+      .getElementById("control_frequency")
+      .addEventListener("input", function (event) {
+        if (oscillatorNode) {
+          oscillatorNode.frequency.value = event.target.value;
+        }
+      });
 
-      document
-        .getElementById("control_frequency")
-        .addEventListener("input", function (event) {
-          if (oscillatorNode) {
-           oscillatorNode.frequency.value = event.target.value;
-          }
-        });
-    
-    
-    document.getElementById('distortion').addEventListener('input', function () {
-      let val = parseInt(this.value* 500);
-      distortion.curve = makeDistortionCurve(val);
-      distortion.oversample = '4x';
-      source.connect(distortion);
-      distortion.connect(context.destination)
-    })
+    document
+      .getElementById("distortion")
+      .addEventListener("input", function () {
+        let val = parseInt(this.value * 500);
+        distortion.curve = makeDistortionCurve(val);
+        distortion.oversample = "4x";
+      });
 
-    
-    document.getElementById('filter').addEventListener('input', function () {
+    document.getElementById("filter").addEventListener("input", function () {
       biquadFilter.type = "lowshelf";
       biquadFilter.frequency.value = this.value;
       biquadFilter.gain.setValueAtTime(10, context.currentTime);
-    })
+    });
 
-    
-        
-    document.getElementById('delay').addEventListener('input', function () {
+    document.getElementById("delay").addEventListener("input", function () {
       source.stop();
-      musicDelay.delayTime.setValueAtTime(this.value, context.currentTime+2);
-     
-      if (this.value > 0) {
+      musicDelay.delayTime.setValueAtTime(this.value, context.currentTime + 2);
 
+      if (this.value > 0) {
         source = context.createBufferSource();
         source.buffer = buffer;
         source.loop = false;
         if (fast_rate > 1 && fast_rate < 1) {
-         source.playbackRate.value = fast_rate;
+          source.playbackRate.value = fast_rate;
         }
-       
-        source.connect(gainNode).connect(panner).connect(musicDelay).connect(biquadFilter).connect(convolverNode).connect(context.destination);
+
+        source
+          .connect(gainNode)
+          .connect(panner)
+          .connect(musicDelay)
+          .connect(biquadFilter)
+          .connect(convolverNode)
+          .connect(context.destination);
         source.start(0);
         setAudioVisualization(source, context);
       }
-    })
-    
-  
+    });
 
     let playBut = document.getElementById("play");
     playBut.addEventListener(
       "click",
       function () {
-        console.log('this', this.dataset.playing)
-       
+        console.log("this", this.dataset.playing);
+        let timerSetting = null;
         if (this.dataset.playing == "true") {
           this.dataset.playing = false;
           context.suspend();
+          clearInterval(timerSetting);
         } else {
           this.dataset.playing = true;
-           if (context.state === 'suspended') {
-          context.resume();
-           } else {
-           source.start();
-           } 
-          
-          
-         
+          if (context.state === "suspended") {
+            context.resume();
+          } else {
+            source.start();
+          }
+
+          timerSetting = setTimeout(function run() {
+            let currentTime =
+              fast_rate > 1 && fast_rate < 1
+                ? context.currentTime.toFixed(2) * fast_rate
+                : context.currentTime.toFixed(2);
+            let stopTime = buffer.duration.toFixed(2);
+            if (currentTime >= 0 && currentTime <= stopTime) {
+              setMusicTimeInfo(currentTime, stopTime);
+              setTimeout(run, 100);
+            } else {
+              clearTimeout(timerSetting);
+            }
+          }, 100);
         }
       },
       false
     );
 
-     source.connect(gainNode).connect(panner).connect(musicDelay).connect(biquadFilter).connect(convolverNode).connect(context.destination);
+    source
+      .connect(gainNode)
+      .connect(panner)
+      .connect(musicDelay)
+      .connect(biquadFilter)
+      .connect(convolverNode)
+      .connect(context.destination);
     setAudioVisualization(source, context);
-   });
+  });
+}
+function printTime(from, to) {
+  let current = from;
+}
+function setMusicTimeInfo(from, to) {
+  document.getElementById("currentTime").textContent = getTimeStamp(from);
+  document.getElementById("durationTime").textContent = getTimeStamp(to);
 }
 
 function setAudioVisualization(source, context) {
@@ -195,31 +217,39 @@ function setAudioVisualization(source, context) {
 
     canvasCtx.fillStyle = "rgb(245,245,245)";
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    
+
     x = 0;
 
     for (var i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i];
-          
-          canvasCtx.fillStyle = 'rgb(4,81,' + (barHeight + 100) + ')';
-          canvasCtx.fillRect(x, HEIGHT - barHeight/2, barWidth, barHeight/2);
-  
-          x += barWidth + 1;
-      }
+      barHeight = dataArray[i];
+
+      canvasCtx.fillStyle = "rgb(4,81," + (barHeight + 100) + ")";
+      canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+      x += barWidth + 1;
+    }
   }
   draw();
 }
 
- function makeDistortionCurve(amount) {
-    let k = typeof amount === "number" ? amount : 50,
-      n_samples = 44100,
-      curve = new Float32Array(n_samples),
-      deg = Math.PI / 180,
-      i = 0,
-      x;
-    for (; i < n_samples; ++i) {
-      x = (i * 2) / n_samples - 1;
-      curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
-    }
-    return curve;
+function makeDistortionCurve(amount) {
+  let k = typeof amount === "number" ? amount : 50,
+    n_samples = 44100,
+    curve = new Float32Array(n_samples),
+    deg = Math.PI / 180,
+    i = 0,
+    x;
+  for (; i < n_samples; ++i) {
+    x = (i * 2) / n_samples - 1;
+    curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
   }
+  return curve;
+}
+
+function getTimeStamp(time) {
+  let hour = parseInt(time / 3600);
+  let min = parseInt((time % 3600) / 60);
+  let sec = parseInt((time % 3600) % 60);
+
+  return `${hour}:${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec-}`;
+}
